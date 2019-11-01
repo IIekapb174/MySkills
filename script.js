270130d50513243c1de2eea945a7684c4e5e03e7
IIekapb174 <35567603+IIekapb174@users.noreply.github.com>
2019-11-01 13:33:42 +0500

    Create pop-up

--- script.js
+++ script.js
@@ -10,3 +10,15 @@ $('html, body').stop().animate({
  window.location.hash = target;
  });
  });
+
+
+ var c = 0;
+ function pop() {
+   if(c == 0) {
+     document.getElementById("box").style.display = "block";
+     c = 1;
+   } else {
+     document.getElementById("box").style.display ="none";
+     c = 0;
+   }
+ }
